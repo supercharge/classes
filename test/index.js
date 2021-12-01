087e18f5e37d78d4c33d039b1490d6a1ac18314d
Marcus Pöhls <mp@seda.digital>
2021-12-01 10:21:42 +0100

    replace jest with uvu and c8

--- test/index.js
+++ test/index.js
@@ -1,68 +1,70 @@
 'use strict'
 
+const { test } = require('uvu')
+const expect = require('expect')
 const { isConstructor, isNotConstructor, isClass, isFunction, isSubclassOf, className } = require('..')
 
-describe('Classes', () => {
-  it('isConstructor', () => {
-    expect(isConstructor(class Rabbit { })).toBe(true)
+test('isConstructor', () => {
+  expect(isConstructor(class Rabbit { })).toBe(true)
 
-    expect(isConstructor(Array)).toBe(false)
-    expect(isConstructor('string')).toBe(false)
-    expect(isConstructor(function classLike () { })).toBe(false)
-  })
-
-  it('isNotConstructor', () => {
-    expect(isNotConstructor(class Rabbit { })).toBe(false)
-
-    expect(isNotConstructor(Array)).toBe(true)
-    expect(isNotConstructor('string')).toBe(true)
-    expect(isNotConstructor(function classLike () { })).toBe(true)
-  })
-
-  it('isClass', () => {
-    expect(isClass(class Rabbit { })).toBe(true)
-
-    expect(isClass(Array)).toBe(false)
-    expect(isClass('string')).toBe(false)
-    expect(isClass(function classLike () { })).toBe(false)
-  })
-
-  it('isFunction', () => {
-    expect(isFunction(Array)).toBe(true)
-    expect(isFunction(class Rabbit { })).toBe(true)
-    expect(isFunction(function func () { })).toBe(true)
-    expect(isFunction(async function func () { })).toBe(true)
-
-    expect(isFunction(1)).toBe(false)
-    expect(isFunction('string')).toBe(false)
-  })
-
-  it('isSubclassOf', () => {
-    class Base { }
-    class Middle extends Base { }
-    class Sub extends Middle { }
-
-    expect(isSubclassOf(Sub, Base)).toBe(true)
-    expect(isSubclassOf(Sub, Middle)).toBe(true)
-
-    expect(isSubclassOf(Base, Sub)).toBe(false)
-    expect(isSubclassOf(Base, Middle)).toBe(false)
-    expect(isSubclassOf(1, 'Sub')).toBe(false)
-    expect(isSubclassOf(1, 2)).toBe(false)
-  })
-
-  it('className', () => {
-    class Base { }
-    class Middle extends Base { }
-    class Sub extends Middle { }
-
-    expect(className(Sub)).toBe('Sub')
-    expect(className(Middle)).toBe('Middle')
-    expect(className(Base)).toBe('Base')
-
-    expect(() => className()).toThrow('The given input is not a class constructor')
-    expect(() => className(1)).toThrow('The given input is not a class constructor')
-    expect(() => className(null)).toThrow('The given input is not a class constructor')
-    expect(() => className(function () {})).toThrow('The given input is not a class constructor')
-  })
+  expect(isConstructor(Array)).toBe(false)
+  expect(isConstructor('string')).toBe(false)
+  expect(isConstructor(function classLike () { })).toBe(false)
 })
+
+test('isNotConstructor', () => {
+  expect(isNotConstructor(class Rabbit { })).toBe(false)
+
+  expect(isNotConstructor(Array)).toBe(true)
+  expect(isNotConstructor('string')).toBe(true)
+  expect(isNotConstructor(function classLike () { })).toBe(true)
+})
+
+test('isClass', () => {
+  expect(isClass(class Rabbit { })).toBe(true)
+
+  expect(isClass(Array)).toBe(false)
+  expect(isClass('string')).toBe(false)
+  expect(isClass(function classLike () { })).toBe(false)
+})
+
+test('isFunction', () => {
+  expect(isFunction(Array)).toBe(true)
+  expect(isFunction(class Rabbit { })).toBe(true)
+  expect(isFunction(function func () { })).toBe(true)
+  expect(isFunction(async function func () { })).toBe(true)
+
+  expect(isFunction(1)).toBe(false)
+  expect(isFunction('string')).toBe(false)
+})
+
+test('isSubclassOf', () => {
+  class Base { }
+  class Middle extends Base { }
+  class Sub extends Middle { }
+
+  expect(isSubclassOf(Sub, Base)).toBe(true)
+  expect(isSubclassOf(Sub, Middle)).toBe(true)
+
+  expect(isSubclassOf(Base, Sub)).toBe(false)
+  expect(isSubclassOf(Base, Middle)).toBe(false)
+  expect(isSubclassOf(1, 'Sub')).toBe(false)
+  expect(isSubclassOf(1, 2)).toBe(false)
+})
+
+test('className', () => {
+  class Base { }
+  class Middle extends Base { }
+  class Sub extends Middle { }
+
+  expect(className(Sub)).toBe('Sub')
+  expect(className(Middle)).toBe('Middle')
+  expect(className(Base)).toBe('Base')
+
+  expect(() => className()).toThrow('The given input is not a class constructor')
+  expect(() => className(1)).toThrow('The given input is not a class constructor')
+  expect(() => className(null)).toThrow('The given input is not a class constructor')
+  expect(() => className(function () {})).toThrow('The given input is not a class constructor')
+})
+
+test.run()
