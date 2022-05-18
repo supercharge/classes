f46f38e43737abeb63986f75c577e865a7792b5a
Marcus Pöhls <mp@seda.digital>
2022-05-18 13:59:47 +0200

    destructure expect

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { test } = require('uvu')
-const expect = require('expect')
+const { expect } = require('expect')
 const { isConstructor, isNotConstructor, isClass, isClassInstance, isFunction, isSubclassOf, className } = require('..')
 
 test('isConstructor', () => {
